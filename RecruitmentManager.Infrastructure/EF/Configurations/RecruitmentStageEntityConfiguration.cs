abf9622b0cc362965ec177ad8d28ef3764fa353e
TomResz <tomaszres@interia.pl>
2023-10-23 19:22:21 +0200

    .

--- RecruitmentManager.Infrastructure/EF/Configurations/RecruitmentStageEntityConfiguration.cs
+++ RecruitmentManager.Infrastructure/EF/Configurations/RecruitmentStageEntityConfiguration.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using RecruitmentManager.Domain.Entities;
-using System.Reflection.Emit;
 
 namespace RecruitmentManager.Infrastructure.EF.Configurations;
 
