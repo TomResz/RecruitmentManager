41d248a6cd644111675ab4c95fc06ad56deb623f
TomResz <tomaszres@interia.pl>
2024-01-07 20:45:19 +0100

    completedproject

--- RecruitmentManager/DI/LanguageExtension.cs
+++ RecruitmentManager/DI/LanguageExtension.cs
@@ -8,14 +8,7 @@ public static class LanguageExtension
 {
 	public static IServiceCollection AddLanguage(this IServiceCollection services)
 	{
-		if (CultureInfo.CurrentCulture.Name == "pl-PL")
-		{
-			services.AddSingleton<ISubtitles, PolishSubtitles>();
-		}
-		else
-		{
-			services.AddSingleton<ISubtitles, EnglishSubtitles>();
-		}
+		services.AddSingleton<ISubtitles, PolishSubtitles>();
 		return services;
 	}
 }
